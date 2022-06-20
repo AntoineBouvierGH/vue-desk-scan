638f1dd7adb0fa128b325528b00ca64a15df5cb9
Antoine Bouvier <abouvier@advanced-schema.com>
2022-06-20 14:22:26 +0200

    add working qrcode reader

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,6 +2,9 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <qrcode-stream @decode="onDecode"></qrcode-stream>
+<qrcode-drop-zone></qrcode-drop-zone>
+<qrcode-capture></qrcode-capture>
   </div>
 </template>
 
@@ -9,10 +12,20 @@
 import { Component, Vue } from 'vue-property-decorator';
 import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
+import { QrcodeStream, QrcodeDropZone, QrcodeCapture } from 'vue-qrcode-reader'
+
 @Component({
   components: {
     HelloWorld,
+    QrcodeStream,
+    QrcodeDropZone,
+    QrcodeCapture
   },
+  methods: {
+  onDecode (decodedString) {
+    console.log("-->", decodedString)
+  }
+}
 })
 export default class HomeView extends Vue {}
 </script>
